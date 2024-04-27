670a3a8347271e1ab3261c9013324fc7032038fb
npm-shubham <shubhambiswas487@gmail.com>
2024-04-27 21:04:47 +0530

    ui updated

--- Milestone3/Socket/hotel.js
+++ Milestone3/Socket/hotel.js
@@ -6,13 +6,6 @@ const hotelSchema = new mongoose.Schema({
     price_per_night: { type: Number },
 }, { timestamps: true });
 
-hotelSchema.post('save', function(doc, next) {
-  // Emit the newly saved hotel to connected clients
-  io.emit('newHotel', doc);
-  next();
-});
-
-
 const Hotel = mongoose.model('Hotel', hotelSchema);
 
 module.exports = Hotel;
